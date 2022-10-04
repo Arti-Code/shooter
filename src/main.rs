734fab63b18f96342f1a528ef352d5b733bd0952
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-10-04 11:12:02 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![allow(unused)]
+
+use bevy::prelude::*;
+
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Capitan Arturo".to_string(),
+            width: 598.0,
+            height: 676.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
+}
