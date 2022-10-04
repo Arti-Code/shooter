9b99584d484ece4b2482d976f4344e8bd1681b68
Arturo <41492101+Arturr0@users.noreply.github.com>
2022-10-04 12:53:25 +0200

    sprite

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 use bevy::prelude::*;
 
+const PLAYER_SPRITE: &str = "player_a_01.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
 
 fn main() {
     App::new()
@@ -13,5 +15,25 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
         .run();
+}
+
+fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    //* add camera
+    commands.spawn_bundle(Camera2dBundle::default());
+    //* add rect
+//    commands.spawn_bundle(SpriteBundle {
+//        sprite: Sprite {
+//            color: Color::rgb(0.25, 0.25, 0.75),
+//            custom_size: Some(Vec2::new(150., 150.)),
+//            ..Default::default()
+//        },
+//        ..Default::default()
+//    });
+    //* add sprite
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load(PLAYER_SPRITE),
+        ..Default::default()
+    });
 }
